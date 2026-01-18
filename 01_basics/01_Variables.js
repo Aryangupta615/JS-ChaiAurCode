706d786c6d11d04d7da8e69be976af9f0036ac29
Aryangupta615 <aryan.gupta4_cs23@gla.ac.in>
2026-01-18 14:36:16 +0000

    THIS IS ALL ABOUT VARIABLES IN JAVASCRIPT

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,7 @@
+const accountId = 1343344
+let emailId = "aryan@gmail.com"
+var accountpass  = "12345"
+accountstatus = "Nil"
+let nomineeId;
+console.table([accountId,emailId,accountpass,accountstatus,nomineeId])
+// prefer not to use var in this javascript language 
